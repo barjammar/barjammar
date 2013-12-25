886813b9783e0d9f617fa29549bc629047afeb8d
Barry Marshall <barry@bajama.(none)>
2013-12-25 20:50:43 +0800

    ddqq

--- bin/make_album.sh
+++ bin/make_album.sh
@@ -5,3 +5,4 @@ CAMDIR=/home/barry/Webcam_Pictures
 echo "######### Starting Album Creation ###########" >> $LOGFILE
 echo "Running cron-job to generate kangaroocam images at $(date)" >> $LOGFILE
 album -sort=name -reverse_sort $CAMDIR 2>&1 >> $LOGFILE
+echo "######### Finished Album Creation ###########" >> $LOGFILE
