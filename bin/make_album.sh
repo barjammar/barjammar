bdadcc6aeb51d242faf1db44cb1409a4a7844a50
Barry Marshall <barry@bajama.(none)>
2013-12-25 20:44:32 +0800

    Barry added the make_album shell script which had changes to sort images by name and do a reverse sort. TODO - next is to add meta refresh to index page header. TODO - remove IE cache, which is Cache-Control: no-cache

---
+++ bin/make_album.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# recommended way to write the cron details
+LOGFILE=/home/barry/album.log
+CAMDIR=/home/barry/Webcam_Pictures
+echo "######### Starting Album Creation ###########" >> $LOGFILE
+echo "Running cron-job to generate kangaroocam images at $(date)" >> $LOGFILE
+album -sort=name -reverse_sort $CAMDIR 2>&1 >> $LOGFILE
