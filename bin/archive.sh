63341188b6c2ce2992e132952346580d082d5399
Barry Marshall <barry@bajama.(none)>
2013-12-28 09:39:10 +0800

    Merged functionaility in archiveDirs.sh into archive.sh. Removed archiveDirs.sh Updated archive.sh to create sortable album directories using Year-Month-Day-DayofWeek

--- bin/archive.sh
+++ bin/archive.sh
@@ -3,9 +3,11 @@
 # archive script 
 ################################################################ 
 ## Config
-LOGFILE='/home/barry/archive.log'
-SCR='/home/barry/Webcam_Pictures'
-DATE=`date --date "1 days ago" "+%A-%d-%m-%y"`
+HOMEDIR='/home/barry'
+LOGFILE='$HOMEDIR/archive.log'
+SCR='$HOMEDIR/Webcam_Pictures'
+DATE=`date --date "1 days ago" "+%y-%m-%d-%A"`
+DAYOFWEEK=`date +%u`
 
 echo "###### Starting Archive Job #######" >> $LOGFILE
 echo "archive.sh at $(date)" >> $LOGFILE
@@ -13,5 +15,13 @@ mkdir "$SCR/$DATE"
 for i in `ls $SCR/*.jpeg`; do
 	mv "$i" "$SCR/$DATE/"
 done
+# On Sunday we archive all of the week
+if [ $DAYOFWEEK eq '7' ]; then
+	if [ ! -d $SCR/archive ]; then
+		mkdir $SCR/archive
+	fi
+	find $SCR/. -type d -name "^1*" -exec mv {} $SRC/archive/ \;
+fi
+$HOMEDIR/make_album.sh >> $LOGFILE
 ## done
 echo "###### Finished Archive Job at $(date) ######" >> $LOGFILE
